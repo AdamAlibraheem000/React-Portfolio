316f1a4ef46acd15150b8955346bd380aed0750f
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-06-19 11:35:48 -0400

    update homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Home() {
   return (
     <div className="home-styling">
-      <h1 className='name-styling'>Adam Al-ibraheem</h1>
       <h1 className='title-styling'>Full Stack Developer</h1>
+      
+      <h1 className='name-styling'>Adam Al-ibraheem</h1>
       <p className='about-styling'>Passionate Full Stack Developer. Former Information Systems graduate from Wright State University and Ohio State University Coding Bootcamp alumni. Continuing desire to learn new technologies and solve problems within the field of development.</p>
     </div>
   )
