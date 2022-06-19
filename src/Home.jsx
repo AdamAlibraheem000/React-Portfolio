6b5a15eefabcb4ab42bf10360afb905b150af24c
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-06-19 13:19:18 -0400

    updated portfolio section

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,15 +6,33 @@ function Home() {
 
 
   return (
+    <>
+    <div className='nav-styling'>
+    <nav className="menu-wrap">
+        <input type="checkbox" className="toggler"></input>
+        <div className="hamburger"><div></div></div>
+        <div className="menu">
+            <div>
+                <div>
+                    <ul>
+                    <li><a href="https://github.com/AdamAlibraheem000" target="_blank" rel="noreferrer noopener" >Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/adam-al-ibraheem/" target="_blank" rel="noreferrer noopener">LinkedIn</a></li>
+                    <li><a href="https://drive.google.com/file/d/10m1LROaTsC4cUsUg8q-hBZxRT9yeA8ej/view?usp=sharing" target="_blank" rel="noreferrer noopener" >Resume</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    </div>
     <div className="home-styling">
       <h1 className='title-styling'>Full Stack Developer</h1>
-      
       <h1 className='name-styling'>Adam Al-ibraheem</h1>
       <p className='about-styling'>Passionate Full Stack Developer. Former Information Systems graduate from Wright State University and Ohio State University Coding Bootcamp alumni. Continuing desire to learn new technologies and solve problems within the field of development.</p>
       <Link to="/Portfolio" className='port-btn'>Portfolio</Link>
 
       
     </div>
+    </>
   )
 }
 
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import sweeneys from './imgs/sweeneys.JPG'
 import jobHunter from './imgs/jobHunter.JPG'
 import pinItPic from './imgs/pinItPic.JPG'
@@ -6,13 +7,15 @@ import weatherDashBoard from './imgs/weatherDashBoard.JPG'
 
 function Portfolio() {
   return (
+    <>
     <section className='portfolio-flex'>
+    <Link to="/" className='port-btn'>Home</Link>
         <div id='port-title'>
             <h1 className="port-h1">Portfolio</h1>
         </div>
         <div className='img-grid'>
             <div><a href="https://sweeney-seafood.herokuapp.com/" target="_blank" rel='noreferrer noopener'>
-                <img src="./assets/imgs/sweeneys.JPG" alt="sweeney's" />
+                <img src={sweeneys} alt="sweeney's" />
                 <h3 >Sweeney's Seafood Bar & Grill</h3>
                 <p className='p-modify p-font'>Updated local restaurant's website using the MERN stack.</p>
                 <p className="p-font">React Express.js Node.js MongoDB GraphQL</p>
@@ -20,7 +23,7 @@ function Portfolio() {
             </div>
             <div>
                 <a href="https://damp-atoll-38354.herokuapp.com/" target="_blank" rel='noreferrer noopener'>
-            <img src="./assets/imgs/pinItPic.JPG" alt="pinIt" />
+            <img src={pinItPic} alt="pinIt" />
                 <h3>Pin-It!</h3>
                 <p className="p-font">An idea board where users can look at ideas to get inspiration for their next big project or trip.</p>
                 <p className="p-font">Node.js Express.js Sequelize MySQL Handlebars.js</p>
@@ -28,14 +31,25 @@ function Portfolio() {
             </div>
             <div>
                 <a href="https://ileachy.github.io/Job-hunter/" target="_blank" rel='noreferrer noopener'>
-                <img src="./assets/imgs/jobHunter.JPG" alt="jobHunter" />
+                <img src={jobHunter} alt="jobHunter" />
                 <h3>The Job Hunter</h3>
                 <p className="p-font">Job search engine that allows a client to search for both civilian and government/military jobs in one place by using job title and location information.</p>
                 <p className="p-font">HTML CSS Javascript Third-Party APIs</p>
                 </a>
             </div>
+            <div><a href="https://adamalibraheem000.github.io/weather-dashboard/" target="_blank" rel='noreferrer noopener'>
+                <img src={weatherDashBoard} alt="weatherDashBoard" />
+                <h3 >Weather DashBoard</h3>
+                <p className='p-modify p-font'>Weather App that pulls forecast data from Open Weather API</p>
+                <p className="p-font">HTML CSS Javascript Third-Party API</p>
+                </a>
+            </div>
+            
         </div>
+        
+        
     </section>
+    </>
   )
 }
 
