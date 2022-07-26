0a2704140466a477d58b523ccc8623ee1d3a1521
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-07-26 11:28:50 -0400

    updated resume link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar() {
                     <ul id='menu-flex'>
                     <li><a id='linkOne' href="https://github.com/AdamAlibraheem000" target="_blank" rel="noreferrer noopener" >Github</a></li>
                     <li><a id='linkTwo' href="https://www.linkedin.com/in/adam-al-ibraheem/" target="_blank" rel="noreferrer noopener">LinkedIn</a></li>
-                    <li><a id='linkThree' href="https://drive.google.com/file/d/1qHSOz1To55mWnoBOIEd1WGhPZ8SBT2q6/view?usp=sharing" target="_blank" rel="noreferrer noopener" >Resume</a></li>
+                    <li><a id='linkThree' href="https://drive.google.com/file/d/1dtUxKewXMvsUnDgXtIcRoSLTrbuFWBvk/view?usp=sharing" target="_blank" rel="noreferrer noopener" >Resume</a></li>
                     </ul>
                 </div>
             </div>
